e46d72df99e5cc4f543af50f21c8cc40e3b30aff
SANDROMI02 <101940471+SANDROMI02@users.noreply.github.com>
2022-03-19 20:30:17 +0400

    Create WorldPosition.cpp

---
+++ WorldPosition.cpp
@@ -0,0 +1,39 @@
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	FString ObjectName = GetOwner() ->GetName();
+	FVector ObjectPosition = GetOwner()->GetActorLocation(); //could have used GetActorTransform(adds rotation and scale) // and made it into a string with ToString()
+	
+	UE_LOG(LogTemp, Error, TEXT("%s location in world is: %s"), *GetOwner()->GetName(), *ObjectPosition.ToString());
+
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
